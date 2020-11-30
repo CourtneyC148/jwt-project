dea74fd15f5d8faacd112d13824a9dbf532ee15c
courtneyc148 <courtneyc148@gmail.com>
2020-11-29 23:47:12 -0500

    update lesson 9 -11

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,5 +1,6 @@
 // controller actions
-const User = require('../models/user')
+const User = require('../models/user');
+const jwt = require('jsonwebtoken');
 
 const handleErrors = (err) => {
   console.log(err.message, err.code);
@@ -18,6 +19,13 @@ const handleErrors = (err) => {
   return errors;
 }
 
+const maxAge = 3 * 24 * 60 * 60;
+const createToken = (id) => {
+  return jwt.sign({ id }, 'jwt lesson 11 secret', {
+    expiresIn: maxAge
+  });
+}
+
 module.exports.signup_get = (req, res) => {
     res.render('signup');
   }
@@ -31,7 +39,9 @@ module.exports.signup_get = (req, res) => {
 
     try {
       const user = await User.create({ email, password });
-      res.status(201).json(user);
+      const token = createToken(user._id);
+      res.cookie('jwt', token, { httpOnly: true, maxAge: maxAge * 1000 });
+      res.status(201).json({ user: user._id });
     } 
     catch(err) {
       const errors = handleErrors(err);
